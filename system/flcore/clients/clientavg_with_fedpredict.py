430ba31850ee3d3ad0776005d715d1875c34d233
claudio <claudiogs.capanema@gmail.com>
2024-04-30 07:13:55 -0300

    First commit (PFLib)

---
+++ system/flcore/clients/clientavg_with_fedpredict.py
@@ -0,0 +1,83 @@
+# PFLlib: Personalized Federated Learning Algorithm Library
+# Copyright (C) 2021  Jianqing Zhang
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import copy
+import torch
+import numpy as np
+import time
+from sklearn.preprocessing import label_binarize
+from sklearn import metrics
+from flcore.clients.clientavg import clientAVG
+from utils.privacy import *
+from fedpredict import fedpredict_client_torch
+
+
+class ClientAvgWithFedPredict(clientAVG):
+    def __init__(self, args, id, train_samples, test_samples, **kwargs):
+        super().__init__(args, id, train_samples, test_samples, **kwargs)
+        self.last_training_round = 0
+
+    def train(self, t):
+        self.last_training_round = t
+        return super().train()
+
+
+    def test_metrics(self, global_model, t, T):
+        testloaderfull = self.load_test_data()
+        # self.model = self.load_model('model')
+        # self.model.to(self.device)
+        nt = t - self.last_training_round
+        combinel_model = fedpredict_client_torch(local_model=self.model, global_model=global_model,
+                                  t=t, T=100, nt=nt)
+        self.model = combinel_model
+        self.model.eval()
+
+        test_acc = 0
+        test_num = 0
+        y_prob = []
+        y_true = []
+
+        with torch.no_grad():
+            for x, y in testloaderfull:
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                output = self.model(x)
+
+                test_acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
+                test_num += y.shape[0]
+
+                y_prob.append(output.detach().cpu().numpy())
+                nc = self.num_classes
+                if self.num_classes == 2:
+                    nc += 1
+                lb = label_binarize(y.detach().cpu().numpy(), classes=np.arange(nc))
+                if self.num_classes == 2:
+                    lb = lb[:, :2]
+                y_true.append(lb)
+
+        # self.model.cpu()
+        # self.save_model(self.model, 'model')
+
+        y_prob = np.concatenate(y_prob, axis=0)
+        y_true = np.concatenate(y_true, axis=0)
+
+        auc = metrics.roc_auc_score(y_true, y_prob, average='micro')
+
+        return test_acc, test_num, auc
